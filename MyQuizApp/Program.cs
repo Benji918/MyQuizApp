a8edd137cebdc68e3785350c7c0c937fb1da1312
Benjamin <kodiugos@gmail.com>
2024-09-11 23:29:59 -0500

    Add project files.

---
+++ MyQuizApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MyQuizApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Question[] questions = new Question[]
+            {
+                new Question(question_text:"What is the capital of Nigeria?",
+                             answers: new string[] {"Paris", "Abuja", "Niger"},
+                             correctanswerindex: 1
+                             )
+            };
+            Quiz Quiz = new Quiz(questions);
+            Quiz.DisplayQuestion(questions[0]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyQuizApp/Quiz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQuizApp
+{
+    internal class Quiz
+    {
+        private Question[] questions;
+
+        public Quiz(Question[] questions)
+        {
+            this.questions = questions;
+        }
+
+       public void DisplayQuestion(Question question)
+        {
+            Console.WriteLine(question.QuestionText);
+
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                Console.WriteLine($"{question.Answers[i]}");
+            }
+        }
+    }
+}
