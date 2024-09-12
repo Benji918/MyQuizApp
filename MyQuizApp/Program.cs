a5ae621897ceb38e5e352d989f586e2f5b6fcfec
Benjamin <kodiugos@gmail.com>
2024-09-12 00:46:31 -0500

    Added extra functionality(display results)

--- MyQuizApp/Program.cs
+++ MyQuizApp/Program.cs
@@ -9,10 +9,15 @@
                 new Question(question_text:"What is the capital of Nigeria?",
                              answers: new string[] {"Paris", "Abuja", "Niger"},
                              correctanswerindex: 1
-                             )
+                             ),
+                new Question(question_text:"What is 10 + 10?",
+                             answers: new string[] {"3", "4", "20"},
+                             correctanswerindex: 2
+                             ),
+
             };
             Quiz Quiz = new Quiz(questions);
-            Quiz.DisplayQuestion(questions[0]);
+            Quiz.StartQuiz();
             Console.ReadKey();
         }
     }
--- MyQuizApp/Quiz.cs
+++ MyQuizApp/Quiz.cs
@@ -8,21 +8,112 @@ namespace MyQuizApp
 {
     internal class Quiz
     {
-        private Question[] questions;
+        private Question[] _questions;
+        private byte _score;
 
         public Quiz(Question[] questions)
         {
-            this.questions = questions;
+            this._questions = questions;
         }
 
-       public void DisplayQuestion(Question question)
+        public void StartQuiz()
         {
+            Console.WriteLine("Start Quiz!!!");
+            int questionNumber = 1; // to display question numbers
+
+            foreach (Question question in _questions)
+            {
+                Console.WriteLine($"Question {questionNumber++}");
+                DisplayQuestion(question);
+                int userChoice = GetUserChoice();
+                if (question.IsCorrectAnswer(userChoice))
+                {
+                    Console.WriteLine("Correct");
+                    _score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect. The correct answer is: {question.Answers[question.CorrectAnswerIndex]}");
+                }
+            }
+            DisplayResults();
+        }
+
+        private void DisplayResults()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("╔═════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                                 Results                                 ║");
+            Console.WriteLine("╚═════════════════════════════════════════════════════════════════════════╝");
+            Console.ResetColor();
+
+            Console.WriteLine($"Quiz finished. Your score is: {_score} out of {_questions.Length}");
+
+            double percentage = (double)_score / _questions.Length;
+
+            if (percentage >= 0.8)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Excellent Work");
+            }
+            else if(percentage >= 0.5)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Good Work");
+            }
+            else 
+            {
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Keep Practicing mate!");
+            }
+            Console.ResetColor();
+        }
+
+        private void DisplayQuestion(Question question)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("╔═════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                                 Question                                ║");
+            Console.WriteLine("╚═════════════════════════════════════════════════════════════════════════╝");
+            Console.ResetColor();
             Console.WriteLine(question.QuestionText);
 
             for (int i = 0; i < question.Answers.Length; i++)
             {
-                Console.WriteLine($"{question.Answers[i]}");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(" ");
+                Console.Write(i + 1);
+                Console.ResetColor();
+
+                Console.WriteLine($". {question.Answers[i]}");
             }
+
+            /*if (GetUserChoice() == question.CorrectAnswerIndex)
+            {
+                Console.WriteLine("Your answer is correct!!!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong answer!!!");
+            }*/
+            
+        }
+
+        private int GetUserChoice()
+        {
+            Console.WriteLine("Your anwser is: ");
+            string user_input = Console.ReadLine();
+            int choice = 0;
+            while (!
+                int.TryParse(user_input, out choice) || choice < 1 || choice > 4)
+            {
+                Console.WriteLine("Invalid choice. Enter a choice between 1 and 4!");
+                Console.ReadLine();
+
+            }
+            return choice - 1; // adjust to 0 indexed array
+
         }
     }
 }
